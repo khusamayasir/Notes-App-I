ba429dde9992f93cc9d4e189fbff531805cdf727
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-04 21:17:17 +0500

    update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react"
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <section className="pane-sidebar">
+        
+    </section>
   )
 }
