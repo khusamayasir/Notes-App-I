9c272d6cb73575630078dd96e57c78eab8526078
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-05 04:03:44 +0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react"
+import Sidebar from "./components/Sidebar"
+import Editor from "./components/Editor"
 import "./style.css"
 
 export default function App() {
+  const [notes, setNotes] = React.useState([])
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+      (notes[0] && notes[0].id) || ""
+  )
+  
   return (
     <main>
       <div className="no-notes">
