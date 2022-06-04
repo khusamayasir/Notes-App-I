28be8cb4e4127159f067568280f64cc7842492d8
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-04 14:50:20 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ export default function App() {
     <main>
       <div className="no-notes">
         <h1>You have no notes</h1>
-        <button>
-          
+        <button
+        className="first-note">
+          Create one now
         </button>
       </div>
     </main>
