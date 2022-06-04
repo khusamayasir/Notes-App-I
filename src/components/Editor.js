d7c71b0d5cd0f0585229671fd4ef6f9f0e58027e
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-04 21:24:13 +0500

    update Editor.js

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react"
 
 export default function Editor() {
   return (
-    <div>Editor</div>
+    <section className="pane-editor">
+
+    </section>
   )
 }
