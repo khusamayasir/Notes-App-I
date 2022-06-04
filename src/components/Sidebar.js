453b5dfb92543bda6db6772b8c6e9d76f306a6e8
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-05 03:55:46 +0500

    update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Sidebar() {
   return (
-    <section className="pane-sidebar">
+    <section className="pane sidebar">
         <div className="sidebar--header">
             <h3>Notes</h3>
             <button className="new-note">+</button>
