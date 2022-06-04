bd504a10bb9df3150865c28e0b9e5eb3fcc9268e
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-05 04:05:10 +0500

    update App.js add function

--- src/App.js
+++ src/App.js
@@ -8,7 +8,16 @@ export default function App() {
   const [currentNoteId, setCurrentNoteId] = React.useState(
       (notes[0] && notes[0].id) || ""
   )
-  
+
+  function createNewNote() {
+    const newNote = {
+        id: nanoid(),
+        body: "# Type your markdown note's title here"
+    }
+    setNotes(prevNotes => [newNote, ...prevNotes])
+    setCurrentNoteId(newNote.id)
+  }
+
   return (
     <main>
       <div className="no-notes">
