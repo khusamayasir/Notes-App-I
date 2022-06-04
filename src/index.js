094b1c7334be86b754f70f7a69c0636dc86064a3
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-04 14:43:39 +0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react'
 
-export default function index() {
-  return (
-    <div>index</div>
-  )
-}
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+
+ReactDOM.render(<App />, document.getElementById("root"))
