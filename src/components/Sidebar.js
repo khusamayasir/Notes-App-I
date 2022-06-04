37f649d034ec7585e35e9d9cd2ab50b30ac33ad4
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-04 21:18:17 +0500

    update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,7 +4,8 @@ export default function Sidebar() {
   return (
     <section className="pane-sidebar">
         <div className="sidebar--header">
-            
+            <h3>Notes</h3>
+            <button className="new-note">+</button>
         </div>
     </section>
   )
