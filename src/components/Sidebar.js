e0f7c5b658623ec5833c30bd65a7f53709bd1e14
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-06 23:44:30 +0500

    update Sidebar.js add onClick

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -19,7 +19,7 @@ export default function Sidebar(props) {
     <section className="pane sidebar">
         <div className="sidebar--header">
             <h3>Notes</h3>
-            <button className="new-note">+</button>
+            <button className="new-note" onClick={props.newNote}>+</button>
         </div>
     </section>
   )
