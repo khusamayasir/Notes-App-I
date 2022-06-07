9194309a07bea1103f08d30723965ff5039d1a52
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-08 01:50:32 +0500

    update Sidebar.css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -23,7 +23,7 @@ export default function Sidebar(props) {
 
     return (
         <section className="pane sidebar">
-            <div className="sidebar--header">
+            <div className="sidebar-header">
                 <h3>Notes</h3>
                 <button className="new-note" onClick={props.newNote}>+</button>
             </div>
