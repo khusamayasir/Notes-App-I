bb486ab60d258e22dbd9750b150c74e026e273d9
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-07 12:22:09 +0500

    update Editor.js

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -15,7 +15,17 @@ export default function Editor({ currentNote, updateNote }) {
 
   return (
       <section className="pane editor">
-
+          <ReactMde
+              value={currentNote.body}
+              onChange={updateNote}
+              selectedTab={selectedTab}
+              onTabChange={setSelectedTab}
+              generateMarkdownPreview={(markdown) =>
+                  Promise.resolve(converter.makeHtml(markdown))
+              }
+              minEditorHeight={80}
+              heightUnits="vh"
+          />
       </section>
   )
 }
