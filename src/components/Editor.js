de2390ac64b2d393c90620d1af186a72c5872f6c
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-07 12:21:25 +0500

    update Editor.js

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,10 +1,14 @@
 import React from "react"
 import "./Editor.css"
+import ReactMde from "react-mde"
+import Showdown from "showdown"
 
-export default function Editor() {
+export default function Editor({ currentNote, updateNote }) {
+ 
+
   return (
-    <section className="pane editor">
+      <section className="pane editor">
 
-    </section>
+      </section>
   )
 }
