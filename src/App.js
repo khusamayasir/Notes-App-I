97fc80e23f6a4cbd52aa0c28c072841b478f9e5b
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-09 19:54:32 +0500

    update commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
 import "./style.css"
-import {data}  from "./data"
 import Split from "react-split"
 import {nanoid} from "nanoid"
 
